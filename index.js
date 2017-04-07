1358e72c84208ded4e940a8ccd00aff530a04c37
isayme <isaymeorg@gmail.com>
2017-04-07 14:49:04 +0800

    working version

---
+++ index.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose')
+var errmessage = 'remove function disabled for this schema'
+
+module.exports = function (schema) {
+  // disable Model.remove and document.remove
+  schema.methods.remove = schema.statics.remove = function () {
+    var error = new Error(errmessage)
+
+    var args = Array.prototype.slice.call(arguments)
+    var callback = args[args.length - 1]
+    var isCallbackModel = typeof callback === 'function'
+
+    if (isCallbackModel) {
+      callback(error)
+    } else if (mongoose.Promise) {
+      var MPromise = mongoose.Promise.ES6 || mongoose.Promise
+      return new MPromise(function (resolve, reject) {
+        reject(error)
+      })
+    } else {
+      throw error
+    }
+  }
+}
---
+++ test/index-test.js
@@ -0,0 +1,80 @@
+var assert = require('power-assert')
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema
+var disableRemovePlugin = require('../index')
+
+var TestSchema = new Schema({
+  content: String
+})
+
+TestSchema.plugin(disableRemovePlugin)
+var Test = mongoose.model('Test', TestSchema)
+
+describe(process.env.npm_package_name, function () {
+  var errmessage = 'remove function disabled for this schema'
+
+  describe('Document.remove', function () {
+    it('shuold throw with callback', function (done) {
+      var test = new Test()
+      test.remove(function (err) {
+        assert.ok(err instanceof Error)
+        assert.ok(err.message === errmessage)
+        done()
+      })
+    })
+
+    it('should return a reject promise if without callback', function (done) {
+      var test = new Test()
+      test.remove()
+        .catch(function (err) {
+          assert.ok(err instanceof Error)
+          assert.ok(err.message === errmessage)
+          done()
+        })
+    })
+
+    it('should ok for custom promise', function (done) {
+      var Promise = mongoose.Promise
+      mongoose.Promise = require('bluebird')
+      var test = new Test()
+      test.remove()
+        .catch(function (err) {
+          assert.ok(err instanceof Error)
+          assert.ok(err.message === errmessage)
+          mongoose.Promise = Promise
+          done()
+        })
+    })
+  })
+
+  describe('Model.remove', function () {
+    it('shuold throw with callback', function (done) {
+      Test.remove({}, function (err) {
+        assert.ok(err instanceof Error)
+        assert.ok(err.message === errmessage)
+        done()
+      })
+    })
+
+    it('should return a reject promise if without callback', function (done) {
+      Test.remove({})
+        .catch(function (err) {
+          assert.ok(err instanceof Error)
+          assert.ok(err.message === errmessage)
+          done()
+        })
+    })
+
+    it('should ok for custom promise', function (done) {
+      var Promise = mongoose.Promise
+      mongoose.Promise = require('bluebird')
+      Test.remove()
+        .catch(function (err) {
+          assert.ok(err instanceof Error)
+          assert.ok(err.message === errmessage)
+          mongoose.Promise = Promise
+          done()
+        })
+    })
+  })
+})
